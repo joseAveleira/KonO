37b844cabfcc36c4479e2665778d973d771781fc
joseAveleira <javelm00@estudiantes.unileon.es>
2017-03-13 11:14:51 +0100

    prueba

--- app/componentes/servername.js
+++ app/componentes/servername.js
@@ -9,7 +9,7 @@
         return {
             //en la url la coleccion a la que se accede (Queso,Hierba)
             name: 'http://localhost:11342/' //para local
-           // name: 'http://178.62.82.186/' //para despliegue
+           // name: 'http://178.62.82.186/' //para despliegue servidor
         };
 
     }
